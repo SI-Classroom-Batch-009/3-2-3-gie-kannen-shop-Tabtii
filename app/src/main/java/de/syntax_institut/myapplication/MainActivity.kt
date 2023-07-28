7b56c5264cb20bc27999460f6400bd3173227786
Torben <torben.wolff@live.de>
2023-07-28 14:12:04 +0200

    F

--- app/src/main/java/de/syntax_institut/myapplication/MainActivity.kt
+++ app/src/main/java/de/syntax_institut/myapplication/MainActivity.kt
@@ -1,24 +1,29 @@
 package de.syntax_institut.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import de.syntax_institut.myapplication.adapter.ItemAdapter
 import de.syntax_institut.myapplication.data.ItemDataSource
 import de.syntax_institut.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide();
 
-        // Liste aus Filmtiteln wird von der Datasource geladen
+        // Liste aus Pokemon wird von der Datasource geladen
         val pokeTitles = ItemDataSource().loadData()
-        val pokeAdapter = ItemAdapter(this, pokeTitles)
+        val pokeAdapter = ItemAdapter(this,pokeTitles,)
 
         // recyclerView von Layout wird mit code verknüpft
         binding.pokeRV.adapter = pokeAdapter
-
     }
 }
